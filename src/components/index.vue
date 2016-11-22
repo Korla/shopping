40a62e8db3e181cff8eb7ccec5c6dfc6f0ee7c05
korla <johannes.bengtsson@raysearchlabs.com>
2016-11-22 20:58:41 +0100

    Addl ist

--- src/components/index.vue
+++ src/components/index.vue
@@ -7,7 +7,18 @@
     </div>
 
     <div class="layout-view">
-      Test
+      <div class="layout-padding">
+        <div class="list">
+          <div class="item" v-for="item in current">
+            <div class="item-content">
+              <label>
+                <quasar-checkbox v-model="item.checked"></quasar-checkbox>
+                <input v-model="item.text"/>
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </quasar-layout>
 </template>
@@ -16,6 +27,11 @@
 export default {
   data () {
     return {
+      master: [],
+      current: [
+        { text: 'Apples', checked: false },
+        { text: 'Oranges', checked: false }
+      ]
     }
   },
   methods: {
